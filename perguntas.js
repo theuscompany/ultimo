963d99eac18f390b1f40148fa922775c99bca5e3
theuscompany <168585450+theuscompany@users.noreply.github.com>
2024-11-05 15:34:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Culinária',
+    'Qual é a comida tradicional mais famosa do Japão?',
+    'Sushi 🍣'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Japão?',
+    'Tóquio ⛩'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o nome do famoso monte sagrado do Japão?',
+    'Monte Fuji 🗻'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual arte marcial foi criada no Japão?',
+    'Judô 🥋'
+)
+
+criaCartao(
+    'Cultura',
+    'Qual festival japonês é conhecido pelas flores de cerejeira (Sakura)?',
+    'Hanami 🌸'
+)
+
+criaCartao(
+    'História',
+    'Em que ano o Japão sediou os Jogos Olímpicos pela primeira vez?',
+    '1964 ⌛'
+)
+
+criaCartao(
+    'Tecnologia',
+    'Qual é o nome do trem de alta velocidade no Japão?',
+    'Shinkansen 🚅'
+)
+
+criaCartao(
+    'Moda',
+    'Qual é a vestimenta tradicional japonesa?',
+    'Kimono 👘'
+)
